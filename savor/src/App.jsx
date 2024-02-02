09398123ab1503ff20494378492125ae63333215
injili <nyarekigospel@gmail.com>
2024-02-02 17:56:27 +0300

    first progress report

---
+++ savor/src/App.jsx
@@ -0,0 +1,14 @@
+import Dashboard from './components/Dashboard';
+import Inventory from './components/Inventory'
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>Welcome</h1>
+            </div>
+            < Inventory />
+        </>
+    );
+};
+export default App; 
